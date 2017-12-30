ced0ed7e9b8d17dcf2ecca4b5199a2dacebf87e1
koocbor <robcook@gmail.com>
2017-12-30 16:28:51 -0500

    new attempt at recursive directory scraping

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ var http = require('http');
 
 var picturesDictionary = {};
 
-const FileSystem = require('fs');
-const Path = require('path');
+const fs = require('fs');
+const path = require('path');
+const walkSync = (d) => fs.statSync(d).isDirectory() ? fs.readdirSync(d).map(f => walkSync(path.join(d, f))) : d;
 
 initPicturesDictionary();
 
@@ -16,14 +17,14 @@ http.createServer(function (request, response) {
     // Then select a random .jpg or .jpeg file
     var yearDir = randomIntFromInterval(process.env.PICTURES_START_YEAR, new Date().getFullYear());
 
-    var picturesArray = picturesDictionary[yearDir] || readDirRecursive(process.env.PICTURES_ROOT_DIR + yearDir).filter(fileName => fileName.toLowerCase().endsWith("jpeg") || fileName.toLowerCase().endsWith("jpg"));
+    var picturesArray = picturesDictionary[yearDir] || walkSync(process.env.PICTURES_ROOT_DIR + yearDir).filter(fileName => typeof fileName === 'string' && (fileName.toLowerCase().endsWith("jpeg") || fileName.toLowerCase().endsWith("jpg")));
     var selectedIndex = randomIntFromInterval(0, picturesArray.length);
     var selectedFilePath = picturesArray[selectedIndex];
 
     console.log(selectedFilePath);
 
     try {
-        var fileStream = FileSystem.createReadStream(selectedFilePath);
+        var fileStream = fs.createReadStream(selectedFilePath);
         fileStream.on('open', function() {
             response.setHeader('Content-Type', 'image/jpeg');
             fileStream.pipe(response);
@@ -37,7 +38,8 @@ http.createServer(function (request, response) {
 function initPicturesDictionary() {
     for (var year = process.env.PICTURES_START_YEAR; year < new Date().getFullYear(); year++) {
         console.log('initializing ' + year);
-        picturesDictionary[year] = readDirRecursive(process.env.PICTURES_ROOT_DIR + year).filter(fileName => fileName.toLowerCase().endsWith("jpeg") || fileName.toLowerCase().endsWith("jpg"));
+        var tmp = walkSync(process.env.PICTURES_ROOT_DIR + year);
+        picturesDictionary[year] = walkSync(process.env.PICTURES_ROOT_DIR + year).filter(fileName => typeof fileName === 'string' && (fileName.toLowerCase().endsWith("jpeg") || fileName.toLowerCase().endsWith("jpg")));
     }
 }
 
@@ -47,8 +49,8 @@ function randomIntFromInterval(min,max) {
     return Math.floor(Math.random()*(max-min+1)+min);
 }
 
-function readDirRecursive(dir) {
-    return FileSystem.statSync(dir).isDirectory()
-        ? Array.prototype.concat(...FileSystem.readdirSync(dir).map(f => readDirRecursive(Path.join(dir, f))))
-        : dir;
-}
+// function readDirRecursive(dir) {
+//     return FileSystem.statSync(dir).isDirectory()
+//         ? Array.prototype.concat(...FileSystem.readdirSync(dir).map(f => readDirRecursive(Path.join(dir, f))))
+//         : dir;
+// }
